08ddb4fbcd4f32648dc4ecf83c9dd6427c18e2ba
moruch4nn <moruch4nn@gmail.com>
2023-12-26 03:10:14 +0900

    bug fix.

--- src/main/kotlin/smartexposed/SmartVariable.kt
+++ src/main/kotlin/smartexposed/SmartVariable.kt
@@ -5,29 +5,30 @@ import org.jetbrains.exposed.sql.Column
 import kotlin.reflect.KProperty
 
 fun <T> smartVariable(column: Column<T>): SmartVariable<T, T> {
-    return SmartVariable(column = column) { it }
+    return SmartVariable(column = column, initBlock = { it }, insertBlock = { it })
 }
 
-fun <T, V> smartVariable(column: Column<T>, initBlock: (T) -> V): SmartVariable<T, V> {
-    return SmartVariable(column = column, initBlock = initBlock)
+fun <T, V> smartVariable(column: Column<T>, initBlock: (T) -> V, insertBlock: (V) -> T): SmartVariable<T, V> {
+    return SmartVariable(column = column, initBlock = initBlock, insertBlock = insertBlock)
 }
 
 fun <T> smartVariableWithDefault(column: Column<T>, defaultValue: T): SmartVariableWithDefault<T, T> {
-    return SmartVariableWithDefault(column = column, defaultValue = defaultValue) { it }
+    return SmartVariableWithDefault(column = column, defaultValue = defaultValue, initBlock = { it }, insertBlock = { it })
 }
 
-fun <T, V> smartVariableWithDefault(column: Column<T>, defaultValue: V, initBlock: (T) -> V): SmartVariableWithDefault<T, V> {
-    return SmartVariableWithDefault(column = column, defaultValue = defaultValue, initBlock = initBlock)
+fun <T, V> smartVariableWithDefault(column: Column<T>, defaultValue: V, initBlock: (T) -> V, insertBlock: (V)->T): SmartVariableWithDefault<T, V> {
+    return SmartVariableWithDefault(column = column, defaultValue = defaultValue, initBlock = initBlock, insertBlock = insertBlock)
 }
 
-open class SmartVariableWithDefault<T, V>(column: Column<T>, defaultValue: V, initBlock: (T) -> V): SmartVariable<T, V> (column, initBlock = initBlock) {
+open class SmartVariableWithDefault<T, V>(column: Column<T>, defaultValue: V, initBlock: (T) -> V, insertBlock: (V)->T): SmartVariable<T, V> (column, initBlock = initBlock, insertBlock = insertBlock) {
     init {
         this.value = defaultValue
     } }
 
 open class SmartVariable<T, V>(
     open val column: Column<T>,
-    val initBlock: (T)->V
+    val initBlock: (T)->V,
+    val insertBlock: (V)->T
 ) {
     var value: V? = null
     var initialized = false
